d68f3bd087fae14a4428e34e38c1a3db169a0c09
Sven Haberer <schroenser@gmail.com>
2025-08-27 13:27:04 +0200

    Centered buttons.

--- client.lua
+++ client.lua
@@ -1,26 +1,34 @@
 local serverId = 1
 
+local colors = _G.colors
 local basalt = require("basalt")
 local colorNames = {"white","orange","magenta","lightBlue","yellow","lime","pink","gray","lightGray","cyan","purple","blue","brown","green","red","black"}
 local labels = require("buttons")
 local colorMap = {}
+local longestLabelLength = 0
 
 rednet.open("back")
 
 for _, color in ipairs(colorNames) do
     colorMap[color] = false
+    if labels[color] ~= nil and #labels[color] > longestLabelLength then
+        longestLabelLength = #labels[color]
+    end
 end
+
+local buttonWidth = longestLabelLength + 2
 
 local main = basalt.getMainFrame()
 main:setSize(26, 20):setPosition(1, 1)
+local buttonX = math.floor((main:getWidth() - buttonWidth) / 2) + 1
 
 local btnRow = 1
 for _, color in ipairs(colorNames) do
     if labels[color] ~= nil then
         local btn = main:addButton()
             :setText(labels[color])
-            :setPosition(8, btnRow + 2)
-            :setSize(10, 1)
+            :setPosition(buttonX, btnRow + 2)
+            :setSize(buttonWidth, 1)
             :setBackground(colorMap[color] and colors[color] or colors.gray)
             :onClick(function(self)
                 colorMap[color] = not colorMap[color]
