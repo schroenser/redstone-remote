dae1f2310bfc91bd7005c663da95546ca18b979b
Sven Haberer <schroenser@gmail.com>
2025-08-27 13:15:33 +0200

    Made buttons optional.

--- client.lua
+++ client.lua
@@ -9,25 +9,26 @@ rednet.open("back")
 
 for i, color in ipairs(colorNames) do
     colorMap[color] = false
-    if labels[color] == nil then
-        labels[color] = color
-    end
 end
 
 local main = basalt.getMainFrame()
 main:setSize(26, 20):setPosition(1, 1)
 
-for i, color in ipairs(colorNames) do
-    local btn = main:addButton()
-        :setText(labels[color])
-        :setPosition(8, i + 2)
-        :setSize(10, 1)
-        :setBackground(colorMap[color] and colors[color] or colors.gray)
-        :onClick(function(self)
-            colorMap[color] = not colorMap[color]
-            rednet.send(serverId, {color=color, state=colorMap[color]})
-            self:setBackground(colorMap[color] and colors[color] or colors.gray)
-        end)
+local btnRow = 1
+for _, color in ipairs(colorNames) do
+    if labels[color] ~= nil then
+        local btn = main:addButton()
+            :setText(labels[color])
+            :setPosition(8, btnRow + 2)
+            :setSize(10, 1)
+            :setBackground(colorMap[color] and colors[color] or colors.gray)
+            :onClick(function(self)
+                colorMap[color] = not colorMap[color]
+                rednet.send(serverId, {color=color, state=colorMap[color]})
+                self:setBackground(colorMap[color] and colors[color] or colors.gray)
+            end)
+        btnRow = btnRow + 1
+    end
 end
 
 basalt.run()
